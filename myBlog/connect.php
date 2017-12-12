f52fb5a80cae92201d30370ac6a612c1591ff745
houweix <houwei19960927@126.com>
2017-12-12 10:04:31 +0800

    更新数据库

--- myBlog/connect.php
+++ myBlog/connect.php
@@ -7,7 +7,7 @@
 @$link = mysqli_connect("localhost", "root", "") or die("数据库连接错误");
 
 //        2、选择数据库
-@mysqli_select_db($link, "liuyan2") or die("打开数据库错误");
+@mysqli_select_db($link, "myblog") or die("打开数据库错误");
 
 //        3、设置传输编码
 mysqli_set_charset($link, "UTF8");
